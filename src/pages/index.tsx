0298e35185d7210c72929a3a56df65dd0c3ae539
Ver_Trld <jtoralde08@gmail.com>
2024-04-05 20:58:47 +0800

    npm

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,19 @@
-import {
-  Autocomplete,
-  Box,
-  Button,
-  Flex,
-  Grid,
-  Image,
-  Input,
-  Text,
-  Title,
-} from "@mantine/core";
+import { Flex, Grid, Image, Input, Title, Button, Text } from "@mantine/core";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 
 interface Movie {
   id: number;
   poster_path: string;
+  title: string;
+  release_date: string;
   // Add other properties if needed
 }
 
 export default function Home() {
   const [movieList, setMovieList] = useState<Movie[]>([]);
   const [searchTerm, setSearchTerm] = useState("");
+  const [page, setPage] = useState(1);
 
   const router = useRouter();
 
@@ -29,9 +22,9 @@ export default function Home() {
       try {
         let url;
         if (searchTerm.trim() === "") {
-          url = `https://api.themoviedb.org/3/discover/movie?api_key=be7a1480a6d224eefbbe288a47523559`;
+          url = `https://api.themoviedb.org/3/discover/movie?api_key=be7a1480a6d224eefbbe288a47523559&page=${page}`;
         } else {
-          url = `https://api.themoviedb.org/3/search/movie?api_key=be7a1480a6d224eefbbe288a47523559&query=${searchTerm}`;
+          url = `https://api.themoviedb.org/3/search/movie?api_key=be7a1480a6d224eefbbe288a47523559&query=${searchTerm}&page=${page}`;
         }
 
         const response = await fetch(url);
@@ -44,18 +37,21 @@ export default function Home() {
     };
 
     fetchMovies();
-  }, [searchTerm]);
+  }, [searchTerm, page]);
 
   return (
     <>
-      <Flex direction="column">
+      <Flex
+        direction="column"
+        bg={
+          "linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 35%, rgba(0,212,255,1) 100%)"
+        }
+      >
         <Flex
           direction={"row"}
           style={{
             padding: 20,
             color: "white",
-            background:
-              "linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 35%, rgba(0,212,255,1) 100%)",
           }}
           justify={"space-between"}
           align={"center"}
@@ -71,26 +67,55 @@ export default function Home() {
 
         <Grid pt={50}>
           {movieList.map((movie, index) => (
-            <Grid.Col span={{ base: 6, md: 6, lg: 2 }} key={index}>
-              <Image
-                src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
-                alt=""
-                width={50}
-                height={300}
-                style={{ cursor: "pointer", borderRadius: 10 }} // Apply cursor style here
-                onClick={() => {
-                  console.log(`https://vidsrc.to/embed/movie/${movie.id}`);
-                  router.push({
-                    pathname: "/playMovie",
-                    query: {
-                      id: movie.id,
-                    },
-                  });
-                }}
-              />
+            <Grid.Col span={{ base: 6, md: 2, lg: 2, xl: 2 }} key={index}>
+              {movie.poster_path ? (
+                <Flex
+                  direction={"column"}
+                  style={{ textAlign: "center", color: "white" }}
+                >
+                  <Image
+                    src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+                    alt=""
+                    width={50}
+                    height={300}
+                    style={{ cursor: "pointer", borderRadius: 10 }} // Apply cursor style here
+                    onClick={() => {
+                      console.log(`https://vidsrc.to/embed/movie/${movie.id}`);
+                      router.push({
+                        pathname: "/playMovie",
+                        query: {
+                          id: movie.id,
+                        },
+                      });
+                    }}
+                  />
+
+                  <Title order={6}>
+                    {movie.title} {new Date(movie.release_date).getFullYear()}
+                  </Title>
+                </Flex>
+              ) : (
+                <Text>No poster available</Text>
+              )}
             </Grid.Col>
           ))}
         </Grid>
+        <Flex direction={"row"} justify={"center"} gap={10} p={30}>
+          <Button
+            onClick={() => {
+              setPage((prevPage) => prevPage - 1);
+            }}
+          >
+            Prev
+          </Button>
+          <Button
+            onClick={() => {
+              setPage((prevPage) => prevPage + 1);
+            }}
+          >
+            Next
+          </Button>
+        </Flex>
       </Flex>
     </>
   );
--- src/pages/playMovie/index.tsx
+++ src/pages/playMovie/index.tsx
@@ -3,15 +3,14 @@ import { useRouter } from "next/router";
 import React from "react";
 
 export default function index() {
-  const router = useRouter();
-  const { id } = router.query; // Access 'id' from router.query
+  const { query } = useRouter();
 
   return (
     <>
       <Flex direction={"column"} justify={"center"}>
         <AspectRatio h={500}>
           <iframe
-            src={`https://vidsrc.to/embed/movie/${id}`}
+            src={`https://vidsrc.to/embed/movie/${query.id}`}
             title="YouTube video player"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen
