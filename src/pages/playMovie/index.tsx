7c36a5ce5a41662c1d64df941a4cb97b06be53d0
Ver_Trld <jtoralde08@gmail.com>
2024-04-05 21:06:58 +0800

    patch

--- src/pages/playMovie/index.tsx
+++ src/pages/playMovie/index.tsx
@@ -2,15 +2,18 @@ import { AspectRatio, Flex } from "@mantine/core";
 import { useRouter } from "next/router";
 import React from "react";
 
-export default function index() {
-  const { query } = useRouter();
+// Capitalize the component name
+export default function Index() {
+  // Now we're using the useRouter hook within a React functional component
+  const router = useRouter();
+  const { id } = router.query;
 
   return (
     <>
       <Flex direction={"column"} justify={"center"}>
         <AspectRatio h={500}>
           <iframe
-            src={`https://vidsrc.to/embed/movie/${query.id}`}
+            src={`https://vidsrc.to/embed/movie/${id}`}
             title="YouTube video player"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen
