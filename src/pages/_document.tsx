d9952c16131762342d7c01f165a15e3108431628
Ver_Trld <jtoralde08@gmail.com>
2024-04-08 19:41:41 +0800

    add title meta

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,8 +3,15 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-
+      <Head>
+        <title>
+          CineVerse | Watch Movies Online Free on cineversefree.netlify.app
+        </title>
+        <meta
+          name="description"
+          content="The best place to watch movies online for free with HD quality. No registration is required!"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
